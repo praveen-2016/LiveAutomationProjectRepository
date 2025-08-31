b459c27d2380e2d413a24c3a4c394e2732687089
Team Memeber <praveen.k.nadda@gmail.com>
2025-08-31 12:29:21 +0530

    Updated TC01 and TC02

--- src/test/java/tutorialsninja/register/TC_RF_002.java
+++ src/test/java/tutorialsninja/register/TC_RF_002.java
@@ -18,15 +18,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TC_RF_002
-{
+{   
+	WebDriver driver=null;
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
     @Test
 	public  void amazonInGetPasscodefromGmail() throws InterruptedException
 	{
 		
-		WebDriver driver= new ChromeDriver();
+		  driver= new ChromeDriver();
 	      driver.manage().window().maximize();
 	      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	      driver.get("https://www.amazon.in/");
